ac1284906b9c8db77cea8346c5cb436a89baab7c
corentin <corentin.solal2022@campus-eni.fr>
2022-08-25 14:49:58 +0200

    C l'aprem voici du css

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -22,15 +22,6 @@ class RegistrationFormType extends AbstractType
             ->add('telephone',null,['attr'=>['class' => 'glass-button uk-margin','placeholder'=>'Telephone']])
             ->add('mail',null,['attr'=>['class' => 'glass-button uk-margin','placeholder'=>'Mail']])
             ->add('campus',null,['attr'=>['class' => 'glass-button uk-margin','placeholder'=>'Campus']])
-            ->add('agreeTerms', CheckboxType::class, [
-                'attr'=>['class' =>'uk-checkbox'],
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -45,6 +36,15 @@ class RegistrationFormType extends AbstractType
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
+                    ]),
+                ],
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'attr'=>['class' =>'uk-checkbox'],
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
                     ]),
                 ],
             ])
