8c66d9e25ede203182c34aa93b193c74db231652
corentin <corentin.solal2022@campus-eni.fr>
2022-08-26 17:05:12 +0200

    C l'aprem j'ai bossé eheh

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -15,7 +15,7 @@ class SortieType extends AbstractType
     {
         $builder
             ->add('nom',null,['attr'=>['class' => 'glass-button uk-margin', 'placeholder' => 'Nom de la sortie']])
-            ->add('dateHeureDebut',DateTimeType::class,['widget'=>'single_text','attr'=>['class' => 'glass-button uk-margin']])
+            ->add('dateHeureDebut',DateTimeType::class,['widget'=>'single_text','attr'=>['class' => 'glass-button uk-margin','id' => 'cal']])
             ->add('duree',null,['attr'=>['class' => 'glass-button uk-margin', 'placeholder' => 'Durée']])
             ->add('dateLimiteInscription',DateType::class,['widget'=>'single_text','attr'=>['class' => 'glass-button uk-margin']])
             ->add('nbInscriptionsMax',null,['attr'=>['class' => 'glass-button uk-margin', 'placeholder' => 'Nombre d\'inscrits max']])
@@ -23,7 +23,6 @@ class SortieType extends AbstractType
             ->add('campus',null,['attr'=>['class' => 'glass-button uk-margin', 'placeholder' => 'Campus']])
             ->add('lieu',null,['attr'=>['class' => 'glass-button uk-margin', 'placeholder' => 'Lieu']])
             ->add('etat',null,['attr'=>['class' => 'glass-button uk-margin', 'placeholder' => 'Etat']])
-
         ;
     }
 
