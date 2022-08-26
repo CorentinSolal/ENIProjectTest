80b3b5c4b92955e21b25a90ba6d5db8232df810f
lancelot-eni <108260819+lancelot-eni@users.noreply.github.com>
2022-08-26 14:20:31 +0200

    nouveaux fichiers

---
+++ src/Form/SortieFormType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Sortie;
+use App\Entity\Participant;
+use DateTime;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class SortieFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('campus')
+            ->add('nom')
+            ->add('date_heure_debut', DateType::class, ['label' => 'entre'])
+            ->add('date_heure_debut', DateType::class, ['label' => 'et'])
+            ->add('organisateur', CheckboxType::class, [
+                'label' => 'Sorties dont je suis l\'organisateur/trice' ,
+                'required' => false, 'data' => true
+
+            ])
+            ->add('isParticipant', ChoiceType::class, [
+                'multiple' => true, 'expanded' => true, 'choices' => [
+                    'Sorties auxquelles je suis inscrit/e' => true,
+                    'Sorties auxquelles je ne suis pas inscrit/e' => false,
+                ],
+            ])
+
+            ->add('etat', CheckboxType::class, [
+                'label'    => 'Sorties passées',
+                'required' => false,
+            ])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
