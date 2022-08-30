0854dd7c055fb369a7e3e6ebaabdca885d5c9b4c
arthurdauplay <arthur.dauplay2022@campus-eni.fr>
2022-08-30 16:41:13 +0200

    AD

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -24,13 +24,14 @@ class RegistrationFormType extends AbstractType
             ->add('mail',null,['attr'=>['class' => 'glass-button uk-margin','placeholder'=>'Mail']])
             ->add('campus',null,['attr'=>['class' => 'glass-button uk-margin','placeholder'=>'Campus']])
             ->add('plainPassword', RepeatedType::class,  [
+
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'type' =>PasswordType::class,
-                'first_options'  => ['label' => 'Password'],
-                'second_options' => ['label' => 'Repeat Password'],
+                'first_options'  => ['attr' => ['class' => 'glass-button uk-margin', 'placeholder' => 'Password']],
+                'second_options' => ['attr' => ['class' => 'glass-button uk-margin', 'placeholder' => 'Repeat Password']],
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password', 'class'=>'glass-button uk-margin','placeholder'=>'Password'],
+
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
